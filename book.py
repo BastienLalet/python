ea8f9e706b1bdea0c7565fa7f65b9821d6d22746
bastienlalet <lalet.bastien@gmail.com>
2021-03-14 14:45:43 +0100

    python files 1

--- book.py
+++ book.py
@@ -1,5 +1,6 @@
 from book import Book
-def main():book = Book("TEST")
+def main():
+    book = Book("TEST")
     book.insert_buy(10, 10.0)
     book.insert_sell(120, 12.0)
     book.insert_buy(5, 10.0)
